0a2e1c73d50afa229c4a892e4b08d9c39b06df31
David Gomez <bojacasa@gmail.com>
2025-07-15 14:51:14 -0500

    integracion en contenedores funcional

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,16 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi import FastAPI
 from app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ChatBot API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
